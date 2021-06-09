a45242c9beb296d00566bdc273e7faf4dec6c280
T U <tylerletendre98@gmail.com>
2021-06-09 19:34:58 -0400

    added routes that find one single bag updates a bag and deletes a bag

--- Backend/routes/golfBags.js
+++ Backend/routes/golfBags.js
@@ -7,6 +7,7 @@ router.post('/addGolfBag', async (req,res)=>{
         const golfBag = new GolfBag({
             driver: req.body.driver,
             threeWood: req.body.threeWood,
+            fiveWood: req.body.fiveWood,
             threeHybrid: req.body.threeHybrid,
             ironSet: req.body.ironSet,
             wedges: req.body.wedges,
@@ -15,6 +16,68 @@ router.post('/addGolfBag', async (req,res)=>{
         
         await golfBag.save()
         return res.send(golfBag)
+    }catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+//Get all golf bags
+router.get(`/getGolfbags`, async (req,res)=>{
+    try{
+        const golfbags = await GolfBag.find();
+        return res.send(golfbags);
+    }catch (ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+//Get one golf bag
+router.get(`/getOneBag/:id`, async (req,res)=>{
+    try{
+        const golfbag = await GolfBag.findById(req.params.id);
+        // if(!golfbag){
+        //     return res.status(400).send(`The golfbag with the id "${req.params.id}" does not exist.`);
+        // };
+        return res.send(golfbag);
+    }catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`)
+    }
+});
+
+//delete a golfbag
+
+router.delete(`/deleteGolfBag/:id`, async (req,res)=>{
+    try{
+        const golfBag = await GolfBag.findByIdAndRemove(req.params.id);
+        if(!golfBag)
+        return res.status(400).send(`The golfbag with the id "${req.params.id}" does not exist.`);
+        const golfBags = await GolfBag.find()
+        return res.send(golfBags);
+    }catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`)
+    }
+});
+
+router.put(`/updateGolfBag/:id`, async (req,res)=>{
+    try{
+        const golfBag = await GolfBag.findByIdAndUpdate(req.params.id,
+            {
+            driver: req.body.driver,
+            threeWood: req.body.threeWood,
+            fiveWood: req.body.fiveWood,
+            threeHybrid: req.body.threeHybrid,
+            ironSet: req.body.ironSet,
+            wedges: req.body.wedges,
+            putter: req.body.putter
+            },
+            {new: true}
+        );
+        if(!golfBag)
+            return res.status(400).send(`The golfbag with the id "${req.params.id}"
+            does not exist.`);
+
+            await golfBag.save()
+            return res.send(golfBag)
     }catch(ex){
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
