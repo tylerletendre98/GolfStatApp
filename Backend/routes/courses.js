56b59ab63bf1f378ffdf214fb08e7d130078d7aa
T U <tylerletendre98@gmail.com>
2021-06-26 17:37:08 -0400

    added in basic CRUD routes that for the course features

--- Backend/routes/courses.js
+++ Backend/routes/courses.js
@@ -39,5 +39,65 @@ router.post(`/newCourse`, async(req,res)=>{
     }
 })
 
+router.get(`/getCourses`, async (req,res)=>{
+    try{
+        const courses = await Course.find();
+        res.send(courses)
+    }catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.delete(`/deleteCourse/:courseId`, async (req,res)=>{
+    try{
+        const course = await Course.findById(req.params.courseId)
+        course.remove()
+        return res.send(course)
+    }catch(ex){
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+router.put('/updateCourse/:courseId', async (req,res)=>{
+        try{
+            const updatedCourse = await Course.findByIdAndUpdate(req.params.courseId,
+                {
+                    courseName: req.body.courseName,
+                    coursePar: req.body.coursePar,
+                    privateClub: req.body.privateClub,
+                    holeOnePar: req.body.holeOnePar,
+                    holeTwoPar: req.body.holeTwoPar,
+                    holeThreePar: req.body.holeThreePar,
+                    holeFourPar: req.body.holeFourPar,
+                    holeFivePar: req.body.holeFivePar,
+                    holeSixPar: req.body.holeSixPar,
+                    holeSevenPar: req.body.holeSevenPar,
+                    holeEightPar: req.body.holeEightPar,
+                    holeNinePar: req.body.holeNinePar,
+                    holeTenPar: req.body.holeTenPar,
+                    holeElevenPar: req.body.holeElevenPar,
+                    holeTwelvePar: req.body.holeTwelvePar,
+                    holeThirteenPar: req.body.holeThirteenPar,
+                    holeFourteenPar: req.body.holeFourteenPar,
+                    holeFifteenPar: req.body.holeFifteenPar,
+                    holeSixteenPar: req.body.holeSixteenPar,
+                    holeSeventeenPar: req.body.holeSeventeenPar,
+                    holeEighteenPar: req.body.holeEighteenPar,
+                    yearEstablished: req.body.yearEstablished,
+                    cartsAllowed: req.body.cartsAllowed,
+                    courseImageUrl: req.body.courseImageUrl,
+                    courseLength: req.body.courseLength,
+                    courseAddress: req.body.courseAddress,
+                },
+                {new: true}
+                );
+                await updatedCourse.save()
+                return res.send(updatedCourse)
+        }catch(ex){
+            return res.status(500).send(`Internal Server Error: ${ex}`);
+        }
+})
+
+
 
 module.exports = router;
