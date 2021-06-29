ba2223ac4ce69b3e35ff6ad4849c4cc394b46163
T U <tylerletendre98@gmail.com>
2021-06-28 20:07:05 -0400

    this is the v1 of my project

--- Frontend/golf-stat-app/src/components/Pages/Login/Login.jsx
+++ Frontend/golf-stat-app/src/components/Pages/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./login.css";
-import jwtDecode from "jwt-decode";
 
 const Login = (props) => {
   const [email, setEmail] = useState("");
